94a3c26f5c6af53c02bdf5a1ad0aa531f174c5b1
Ivan Joao Foschini <ivan@dc.ufscar.br>
2017-01-12 10:58:56 -0200

    Documentação de alguns serviços.

---
+++ doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Aplicação Financeira RESTful",
+  "version": "1.0.0",
+  "description": "Documentação dos serviços web da disciplina <b>Web Services e Arquitetura RESTful em Grails</b> do Curso de Pós-Graduação Lato Sensu - Desenvolvimento de Software para Web.",
+  "title": "Aplicação Financeira RESTful - Documentação dos serviços web",
+  "url": "http://localhost:8080",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-01-12T12:56:38.649Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
